2b8191dff4dc8df9ca0f2a83604cc628f366d140
Bruno Michetti <brunomichetti@rootstrap.com>
2020-06-02 16:29:38 -0300

    Apply BEM and change px by em in font-size

--- src/App.js
+++ src/App.js
@@ -1,41 +1,53 @@
-import React from 'react';
-import './App.css';
-import smilies from './assets/smilies.png';
-import i6 from './assets/i6.png';
-import social from './assets/social.png';
-import play from './assets/play.png';
-import appstorebtn from './assets/appstore_button.png';
-import menu_icon from './assets/icons/menu_icon.png';
+import React from "react";
+import "./App.css";
+import smilies from "./assets/smilies.png";
+import i6 from "./assets/i6.png";
+import social from "./assets/social.png";
+import play from "./assets/play.png";
+import appStoreButton from "./assets/appstore_button.png";
+import menuIcon from "./assets/icons/menu_icon.png";
 
 function App() {
   return (
-    <div class='main-div'>
-        <div class='left-container'>
-          <div class='menu'>
-            <img src={menu_icon} alt='menu'></img>
+    <div class="main-div">
+
+        <div class="left-container">
+
+          <div class="left-container__menu">
+            <img src={menuIcon} alt="menu_button"></img>
           </div>
-          <div class='smilies' align='center'>
-            <img src={smilies} alt='smilies'></img>
+
+          <div class="left-container__smilies" align="center">
+            <img src={smilies} alt="smilies"></img>
           </div>
-          <p class='app-title' align='center'>TARGET MVD</p>
-          <p class='app-subtitle' align='center'>Find people near you & connect</p>
-          <p class='description-text' align='center'>Create a  target wherever on the map, specify your interest: 
+
+          <p class="left-container__app-title" align="center">TARGET MVD</p>
+          <p class="left-container__app-subtitle" align="center">Find people near you & connect</p>
+          <p class="left-container__description-text" align="center">Create a  target wherever on the map, specify your interest: 
             Travel, Dating, Music, etc and start conecting with others who share your interest.</p>
+
         </div>
-        <div class='right-container' align='center'>
-          <div class='i6-img'>
-            <img src={i6} alt='i6'></img>
+
+        <div class="right-container" align="center">
+
+          <div class="right-container__i6-img">
+            <img src={i6} alt="i6"></img>
           </div>
-          <div class='app-store-button'>
-            <img src={appstorebtn} alt='app-store-button'></img>
+
+          <div class="right-container__app-store-button">
+            <img src={appStoreButton} alt="app-store-button"></img>
           </div>
-          <div class='social'>
-            <img src={social} alt='social'></img>
+
+          <div class="right-container__social">
+            <img src={social} alt="social"></img>
           </div>
-          <div class='play'>
-            <img src={play} alt='play'></img>
+
+          <div class="right-container__play">
+            <img src={play} alt="play"></img>
           </div>
+
         </div>
+
     </div>
   );
 }
