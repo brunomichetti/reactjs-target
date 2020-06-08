fc6d1301ed0d4bb12ba780d35068d473b298ae2b
Bruno Michetti <brunomichetti@rootstrap.com>
2020-06-08 17:25:37 -0300

    login form and router

--- src/App.js
+++ src/App.js
@@ -1,82 +1,16 @@
 import React from "react";
-import "./App.scss";
-import smilies from "./assets/smilies.png";
-import i6 from "./assets/i6.png";
-import social from "./assets/social.png";
-import play from "./assets/play.png";
-import appStoreButton from "./assets/appstore_button.png";
-import menuIcon from "./assets/icons/menu_icon.png";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./pages/Login"
 
 function App() {
   return (
-    <div class="main-div">
-
-        <div class="left-container">
-
-          <div class="left-container__menu">
-            <img src={menuIcon} alt="menu_button"></img>
-          </div>
-
-          <div class="left-container__smilies" align="center">
-            <img src={smilies} alt="smilies"></img>
-          </div>
-
-          <p class="left-container__app-title" align="center">TARGET MVD</p>
-          <p class="left-container__app-subtitle" align="center">Find people near you & connect</p>
-          <p class="left-container__description-text" align="center">Create a  target wherever on the map, specify your interest: 
-            Travel, Dating, Music, etc and start conecting with others who share your interest.</p>
-          
-          <form align='center' class="left-container__login-form">
-
-            <p class="left-container__login-text">EMAIL</p>
-            <input class="left-container__input" type="email"/>
-
-            <p class="left-container__login-text">PASSWORD</p>
-            <input class="left-container__input" type="password"/>
-
-            <div>
-              <button type="button" class="left-container__login-btn-text">SIGN IN</button>
-            </div>
-
-            <div class="left-container__forgot-pwd">
-              <a href="#" onclick="return false;">Forgot your password?</a>
-            </div>
-            
-            <div class="left-container__facebook">
-              <a href="#" onclick="return false;">CONNECT WITH FACEBOOK</a>
-              <hr class="left-container__hr"></hr>
-            </div>
-          
-
-            <div class="left-container__sign-up">
-              <a href="#" onclick="return false;">SIGN UP</a>
-            </div>
-
-          </form>
-
-        </div>
-
-        <div class="right-container" align="center">
-
-          <div class="right-container__i6-img">
-            <img src={i6} alt="i6"></img>
-          </div>
-
-          <div class="right-container__app-store-button">
-            <img src={appStoreButton} alt="app-store-button"></img>
-          </div>
-
-          <div class="right-container__social">
-            <img src={social} alt="social"></img>
-          </div>
-
-          <div class="right-container__play">
-            <img src={play} alt="play"></img>
-          </div>
-
-        </div>
-
-    </div>
+    <Router>
+        <Switch>
+          <Route path={["/", "/login"]}>
+            <Login />
+          </Route>
+        </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "../App.scss";
+import smilies from "../assets/smilies.png";
+import i6 from "../assets/i6.png";
+import social from "../assets/social.png";
+import play from "../assets/play.png";
+import appStoreButton from "../assets/appstore_button.png";
+import menuIcon from "../assets/icons/menu_icon.png";
+
+function Login() {
+    return (
+      <div class="main-div">
+  
+          <div class="left-container">
+  
+            <div class="left-container__menu">
+              <img src={menuIcon} alt="menu_button"></img>
+            </div>
+  
+            <div class="left-container__smilies" align="center">
+              <img src={smilies} alt="smilies"></img>
+            </div>
+  
+            <p class="left-container__app-title" align="center">TARGET MVD</p>
+            <p class="left-container__app-subtitle" align="center">Find people near you & connect</p>
+            <p class="left-container__description-text" align="center">Create a  target wherever on the map, specify your interest: 
+              Travel, Dating, Music, etc and start conecting with others who share your interest.</p>
+            
+            <form align='center' class="left-container__login-form">
+  
+              <p class="left-container__login-text">EMAIL</p>
+              <input class="left-container__input" type="email"/>
+  
+              <p class="left-container__login-text">PASSWORD</p>
+              <input class="left-container__input" type="password"/>
+  
+              <div>
+                <button type="submit" class="left-container__login-btn-text">SIGN IN</button>
+              </div>
+
+              {/* href="/" until de feature is done */}
+              <div class="left-container__forgot-pwd">
+                <a href="/">Forgot your password?</a>
+              </div>
+              
+              <div class="left-container__facebook">
+                <a href="/">CONNECT WITH FACEBOOK</a>
+                <hr class="left-container__hr"></hr>
+              </div>
+            
+  
+              <div class="left-container__sign-up">
+                <a href="/">SIGN UP</a>
+              </div>
+  
+            </form>
+  
+          </div>
+  
+          <div class="right-container" align="center">
+  
+            <div class="right-container__i6-img">
+              <img src={i6} alt="i6"></img>
+            </div>
+  
+            <div class="right-container__app-store-button">
+              <img src={appStoreButton} alt="app-store-button"></img>
+            </div>
+  
+            <div class="right-container__social">
+              <img src={social} alt="social"></img>
+            </div>
+  
+            <div class="right-container__play">
+              <img src={play} alt="play"></img>
+            </div>
+  
+          </div>
+  
+      </div>
+    );
+  }
+  
+  export default Login;
