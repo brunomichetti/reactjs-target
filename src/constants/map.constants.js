0bbf2cd67d06e9e3480653fb029737d118b5df3b
Bruno Michetti <brunomichetti@rootstrap.com>
2020-06-16 17:16:50 -0300

    homepage empty finished

---
+++ src/constants/map.constants.js
@@ -0,0 +1,5 @@
+export const mapConstants = {
+    INITIAL_ZOOM: 18,
+    INTIAL_LAT: -34.906522, 
+    INITIAL_LONG: -56.1919578
+};
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,49 +1,65 @@
 import React from "react";
-import { useSelector } from "react-redux";
 import "../style/App.scss";
 import "../style/HomePage.scss";
 import ExampleComponent from "react-rounded-image";
 import defaultProfileImg from "../assets/profilePicture.png";
-import { FormattedMessage } from "react-intl"
+import { FormattedMessage } from "react-intl";
+import { Map, TileLayer } from "react-leaflet";
+import { mapConstants } from "../constants/map.constants";
+import menuIcon from "../assets/icons/menu_icon.png";
+import smilies from "../assets/smilies.png";
 
 function HomePage() {
-    //const { user: { email }} = JSON.parse(localStorage.getItem("user"));
-    //const { message } = useSelector(state => state.alert);
-    const user = { username: "usuario_prueba", email: "usuarioprueba.com" }
+    const { user: { email }} = JSON.parse(localStorage.getItem("user"));
+
     return (
         <div className="main-div">
   
             <div className="left-menu">
 
-            <p className="left-menu__app-title">TARGET</p>
+                <div className="left-menu__options">
+                    <img src={menuIcon} alt="menu_button"></img>
+                </div>
 
-            <div className="left-menu__profile-picture">
-                <ExampleComponent
-                    image={defaultProfileImg}
-                    roundedSize="0"
-                    imageWidth="150"
-                    imageHeight="150"
-                />
-            </div>
+                <p className="left-menu__app-title">TARGET</p>
 
-            <p className="left-menu__username">{ user.username }</p>
+                <div className="left-menu__profile-picture">
+                    <ExampleComponent
+                        image={defaultProfileImg}
+                        roundedSize="0"
+                        imageWidth="150"
+                        imageHeight="150"
+                    />
+                </div>
 
-            <div className="left-menu__edit-logout">
-                <a href="/"><FormattedMessage id="homepage.edit.text"/></a> &nbsp; / &nbsp;
-                <a href="/"><FormattedMessage id="homepage.logout.text"/></a>
-            </div>
+                <p className="left-menu__username">{ email }</p>
 
-            <hr className="left-menu__hr"></hr>
+                <div className="left-menu__edit-logout">
+                    <a href="/"><FormattedMessage id="homepage.edit.text"/></a> &nbsp; / &nbsp;
+                    <a href="/"><FormattedMessage id="homepage.logout.text"/></a>
+                </div>
 
-            <p className="left-menu__matches"><FormattedMessage id="homepage.nomatches.text"/></p>
-            
+                <hr className="left-menu__hr"></hr>
+
+                <p className="left-menu__matches"><FormattedMessage id="homepage.nomatches.text"/></p>
+
+                <div className="left-menu__smilies">
+                    <img src={smilies} alt="smilies" className="smilies-img"></img>
+                </div>
      
-          </div>
+        </div>
   
-          <div className="targets-map" align="center">
-  
-          </div>
-  
+        <Map
+            center={{lat: mapConstants.INTIAL_LAT, lng: mapConstants.INITIAL_LONG}}
+            zoom={mapConstants.INITIAL_ZOOM}
+            style={{ width: "70vw", height: "100vh"}}
+        >
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+        </Map>
+
       </div>
     )
 }
