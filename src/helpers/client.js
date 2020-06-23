2332eb9ad5ff1d21dac5f79aab921e69b4223404
Bruno Michetti <brunomichetti@rootstrap.com>
2020-06-23 11:53:28 -0300

    Add .env

--- src/helpers/client.js
+++ src/helpers/client.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+require('dotenv').config();
+
 export default axios.create({
-  baseURL: "http://127.0.0.1:8000/api/v1/",
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     "Content-type": "application/json"
   }
