4ab607340c6fa77392f690bd00628969bbfb3163
Bruno Michetti <brunomichetti@rootstrap.com>
2020-06-30 12:45:09 -0300

    Style Select compontent

--- src/components/users/UserSelect.js
+++ src/components/users/UserSelect.js
@@ -3,6 +3,46 @@ import Select from 'react-select';
 import { FormattedMessage } from 'react-intl';
 
 import './user-form.scss';
+import { black, red } from 'color-name';
+
+const customStyle = {
+  control: (base) => ({
+    ...base,
+    borderColor: 'black',
+    borderRadius: 0,
+    borderStyle: 'solid',
+    width: 247,
+    height: 28,
+    // This line disable the blue border
+    boxShadow: 'none',
+  }),
+
+  menuList: (provided, state) => ({
+    ...provided,
+    fontSize: 14,
+  }),
+
+  option: (provided, state) => ({
+    ...provided,
+    color: black,
+    backgroundColor: state.isFocused ? '#2FBCF7' : null,
+  }),
+
+  placeholder: (provided, state) => ({
+    ...provided,
+    color: black,
+    padding: 49,
+    fontSize: 14,
+  }),
+
+  indicatorSeparator: () => {}, // Remove the "|"
+
+  // Remove the down arrow
+  dropdownIndicator: (defaultStyles) => ({
+    ...defaultStyles,
+    '& svg': { display: 'none' },
+  }),
+};
 
 const UserSelect = ({
   optionsSet,
@@ -12,6 +52,7 @@ const UserSelect = ({
 }) => (
   <div align="center">
     <Select
+      styles={customStyle}
       options={optionsSet}
       className="user-form__text user-form__select"
       onChange={onChangeFunction}
