229010e729439f537250ff3b4558484701f378f0
Bruno Michetti <brunomichetti@rootstrap.com>
2020-07-30 10:26:05 -0300

    Separate style in scss

--- src/components/users/UserImageName.js
+++ src/components/users/UserImageName.js
@@ -4,31 +4,14 @@ import defaultProfileImg from '../../assets/profilePicture.png';
 import './user-profile.scss';
 import { userShape } from '../../constants/shapes';
 
-const UserImageName = ({ user }) => {
-  let showImage = defaultProfileImg;
-  if (user.profile_picture) {
-    showImage = user.profile_picture;
-  }
-
-  return (
-    <div className="user-profile__profile-picture">
-      <div
-        style={{
-          width: 150,
-          height: 150,
-          borderRadius: 100,
-          overflow: 'hidden',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <img src={showImage} width="250" alt="user" />
-      </div>
-      <p className="user-profile__username">{user.name}</p>
+const UserImageName = ({ user: { name, profile_picture } }) => (
+  <div className="user-profile__profile-picture">
+    <div className="user-profile__rounded-image">
+      <img src={profile_picture || defaultProfileImg} width="250" alt="user" />
     </div>
-  );
-};
+    <p className="user-profile__username">{name}</p>
+  </div>
+);
 
 UserImageName.propTypes = {
   user: userShape,
