a45f075c092094f27da42aaa2112572d73a87a3e
Bruno Michetti <brunomichetti@rootstrap.com>
2020-08-17 13:29:24 -0300

    Test Home Page elements with Cypress

---
+++ cypress/integration/homepage-spec.js
@@ -0,0 +1,37 @@
+describe('Test home page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('Test text', () => {
+    cy.contains('TARGET MVD');
+    cy.contains('Find people near you & connect');
+    cy.contains(
+      'Create a target wherever on the map, specify your interest: Travel, Dating, Music, etc and start conecting with others who share your interest.'
+    );
+    cy.contains('EMAIL');
+    cy.contains('PASSWORD');
+    cy.contains('Forgot your password?');
+    cy.contains('CONNECT WITH FACEBOOK');
+    cy.contains('SIGN UP');
+  });
+
+  it('Test login inputs', () => {
+    cy.get('input[name="email"]')
+      .type('fake@email.com')
+      .should('have.value', 'fake@email.com');
+    cy.get('input[name="password"]')
+      .type('fakepassword')
+      .should('have.value', 'fakepassword');
+  });
+
+  it('Test empty inputs', () => {
+    cy.get('button[type="submit"]').click();
+    cy.get('.user-form__alert').then((alerts) => {
+      const emailAlert = alerts[0];
+      const passwordAlert = alerts[1];
+      cy.expect(emailAlert.textContent).to.equal('Email is required.');
+      cy.expect(passwordAlert.textContent).to.equal('Password is required.');
+    });
+  });
+});
